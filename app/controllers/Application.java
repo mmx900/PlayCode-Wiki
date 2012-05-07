f4d98c57db463d659a3a2a1b020c7e613df2c723
mmx900 <mmx900@gmail.com>
2012-05-07 16:17:45 +0900

    리팩터링

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import models.Article;
 import play.*;
 import play.mvc.*;
 
@@ -9,46 +8,15 @@ import views.html.*;
 import play.data.Form;
 
 public class Application extends Controller {
-
 	public static Result index() {
-		return ok(index.render("Your new application is ready."));
+		return ok(index.render("Your new wiki is ready."));
 	}
 
-	public static Result listArticle() {
-		return ok(list.render(Article.all()));
-	}
-
-	public static Result viewArticle(String title) {
-		return ok(view.render(Article.find.where().eq("title", title)
-				.findUnique()));
-	}
-
-	static Form<Article> articleForm = form(Article.class);
-
-	public static Result createArticle() {
-		return ok(create.render(articleForm));
-	}
-
-	public static Result newArticle() {
-		Form<Article> filledForm = articleForm.bindFromRequest();
-		if (filledForm.hasErrors()) {
-			return badRequest(create.render(filledForm));
-		} else {
-			Article.create(filledForm.get());
-			return redirect(routes.Application.listArticle());
-		}
-	}
-
-	public static Result updateArticle(String title) {
+	public static Result loginForm() {
 		return TODO;
 	}
 
-	public static Result deleteArticle(String title) {
+	public static Result login() {
 		return TODO;
-	}
-
-	public static Result deleteArticle(Long id) {
-		Article.delete(id);
-		return redirect(routes.Application.listArticle());
 	}
 }
---
+++ app/controllers/UserApp.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import models.Article;
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+import play.data.Form;
+
+public class UserApp extends Controller {
+	public static Result createUserForm() {
+		return TODO;
+	}
+
+	public static Result createUser() {
+		return TODO;
+	}
+
+	public static Result viewUser() {
+		return TODO;
+	}
+
+	public static Result updateUserForm() {
+		return TODO;
+	}
+
+	public static Result updateUser() {
+		return TODO;
+	}
+
+	public static Result deleteUserForm() {
+		return TODO;
+	}
+
+	public static Result deleteUser() {
+		return TODO;
+	}
+}
