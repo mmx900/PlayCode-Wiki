ca692164aa9ff5be18da138379e800c2f934e934
Setzer Kim <setzer@eyeq.co.kr>
2014-03-04 18:10:09 +0900

    Implement markdown rendering

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var playcodeControllers = angular.module('playcodeControllers', []);
+var playcodeControllers = angular.module('playcodeControllers', ['ngSanitize']);
 
 playcodeControllers.controller('ArticleListCtrl', [ '$scope', '$http',
 	function($scope, $http) {
@@ -23,10 +23,17 @@ playcodeControllers.controller('ArticleCreateCtrl', [ '$scope', '$http',
 		}
 	} ]);
 
+marked.setOptions({
+	highlight: function(code) {
+		return hljs.highlightAuto(code).value;
+	}
+});
+
 playcodeControllers.controller('ArticleDetailCtrl', [ '$scope', '$routeParams', '$http',
 	function($scope, $routeParams, $http) {
 		$http.get('/article/' + $routeParams.title).
 			success(function(data, status, headers, config) {
+				data.content = marked(data.content);
 				$scope.article = data;
 			});
 	} ]);
